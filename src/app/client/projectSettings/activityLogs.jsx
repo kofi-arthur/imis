ddcc3fee1f541c22eee8d4ad81a03b0ca6e83c4d
blackadderIII <skobarnes1@gmail.com>
2025-08-26 13:36:53 +0000

    Refactors ActivityLogs component to fetch and display system logs, enhancing functionality and user experience

--- src/app/client/projectSettings/activityLogs.jsx
+++ src/app/client/projectSettings/activityLogs.jsx
@@ -1,3 +1,15 @@
+// import styles from '../../../styles/app/client/projectSettings/activityLogs.module.css'
+
+// import React from 'react'
+
+// export default function ActivityLogs() {
+//     return (
+//         <div className={styles.comingSoonLayout}>
+//             <h1>Coming Soon</h1>
+//         </div>
+//     )
+// }
+
 import styles from "../../../styles/app/client/projectSettings/logs.module.css";
 
 import { useEffect, useState } from "react";
@@ -6,60 +18,70 @@ import { EmptyComponent } from "../../../components/error";
 import { useProject } from "../../../contexts/projectContext";
 
 import { LoadingComponent } from "../../../../../iticket-new/src/components/loadingComponents";
-import {
-    fetchprojectMembers
-} from "../../../services/api";
+import { fetchprojectMembers, fetchSystemLogs } from "../../../services/api";
+import { formatDateTime } from "../../../utils/conversions";
 
-const Log = () => {
-    return (
-        <div className={styles.log}>
-            <p>Samuel Barnes invited you to a project</p>
-            <span>August 12, 2025</span>
-        </div>
-    );
+const Log = ({data}) => {
+  return (
+    <div className={styles.log}>
+      <p>
+        {data.actor?.displayName} {data.details}
+      </p>
+      <span>{formatDateTime(data.date)}</span>
+    </div>
+  );
 };
 
 export default function ActivityLogs() {
-    const { project } = useProject();
-    const [projectMembers, setprojectMembers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+  const { project } = useProject();
+  const [projectMembers, setprojectMembers] = useState([]);
+  const [systemLogs, setsystemLogs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        async function getMembers(projectId) {
-            const res = await fetchprojectMembers(projectId);
-            setprojectMembers(res);
-            setIsLoading(false);
-        }
+  useEffect(() => {
+    async function getMembers(projectId) {
+      const res = await fetchprojectMembers(projectId);
+      setprojectMembers(res);
+      setIsLoading(false);
+    }
 
-        getMembers(project.projectId);
-    }, [project]);
+    getMembers(project.projectId);
+  }, [project]);
 
-    // invite functionality
-    const [isInvitingMember, setIsInvitingMember] = useState(false);
-    return (
-        <section className={styles.content}>
-            <div className={styles.titleContainer}>
-                <h3 className={styles.title}>
-                    Activity Logs
-                </h3>
+  useEffect(() => {
+    async function getLogs(projectId) {
+      const res = await fetchSystemLogs(projectId);
+      setsystemLogs(res);
+      setIsLoading(false);
+    }
 
-                <div className={styles.actions}>
-                    <button className={styles.downloadBtn}>
-                        <i className="fal fa-arrow-down-to-line"></i>
-                        <p>Download</p>
-                    </button>
-                </div>
-            </div>
+    getLogs(project.projectId);
+  }, [project]);
 
-            <section className={styles.membersContainer}>
-                <div className={styles.titles}>
-                    <span className={styles.title}>Logs</span>
-                    <span className={styles.date}>Date Created</span>
-                </div>
+  // invite functionality
+  const [isInvitingMember, setIsInvitingMember] = useState(false);
+  return (
+    <section className={styles.content}>
+      <div className={styles.titleContainer}>
+        <h3 className={styles.title}>Activity Logs</h3>
 
-                <div className={styles.membersList}>
-                    {isLoading && <LoadingComponent width={35} height={35} />}
-                    {/* {!isLoading &&
+        <div className={styles.actions}>
+          <button className={styles.downloadBtn}>
+            <i className="fal fa-arrow-down-to-line"></i>
+            <p>Download</p>
+          </button>
+        </div>
+      </div>
+
+      <section className={styles.membersContainer}>
+        <div className={styles.titles}>
+          <span className={styles.title}>Logs</span>
+          <span className={styles.date}>Date Created</span>
+        </div>
+
+        <div className={styles.membersList}>
+          {isLoading && <LoadingComponent width={35} height={35} />}
+          {/* {!isLoading &&
                         projectMembers.length !== 0 &&
                         projectMembers
                             .sort((a, b) => a.displayName.localeCompare(b.displayName))
@@ -76,15 +98,19 @@ export default function ActivityLogs() {
                                     }}
                                 />
                             ))} */}
-                    <Log />
-                    {!isLoading && projectMembers.length === 0 && (
-                        <EmptyComponent
-                            title={"No Logs Yet"}
-                            message={"These will automatically be created when you start a project."}
-                        />
-                    )}
-                </div>
-            </section>
-        </section>
-    );
+          {!isLoading &&
+            systemLogs.length !== 0 &&
+            systemLogs.map((log, index) => <Log key={index} data={log} />)}
+          {!isLoading && systemLogs.length === 0 && (
+            <EmptyComponent
+              title={"No Logs Yet"}
+              message={
+                "These will automatically be created when you start a project."
+              }
+            />
+          )}
+        </div>
+      </section>
+    </section>
+  );
 }
